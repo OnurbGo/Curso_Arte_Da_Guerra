8896795c8b2048c5bb7e0f842a250e1485cea0fa
Jujusempix <julio.guilherme@grupointegrado.br>
2025-02-26 22:40:21 -0300

    user semi-completo

--- Arte_Da_Guerra/src/controllers/UserController.ts
+++ Arte_Da_Guerra/src/controllers/UserController.ts
@@ -15,8 +15,58 @@ export const getUserById = async (
 };
 
 export const createUser = async (req: Request, res: Response) => {
-  const { name, email, password, type } = req.body;
+  try {
+    const { name, email, password, type, registration_date } = req.body;
 
-  const user = await UserModel.create({ name, email, password, type });
-  res.status(201).json(user);
+    if (!name || name === "") {
+      return res.status(400).json({ error: "Name is Required" });
+    }
+
+    if (!email || email === "") {
+      return res.status(400).json({ error: "Email is Required" });
+    }
+
+    if (!password || password === "") {
+      return res.status(400).json({ error: "Password is Required" });
+    }
+
+    if (!type || type === "") {
+      return res.status(400).json({ error: "Type is Required" });
+    }
+
+    const user = await UserModel.create({
+      name,
+      email,
+      password,
+      type,
+      registration_date,
+    });
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json("Erro interno no Servidor: " + error);
+  }
+};
+
+export const updateUser = async (
+  req: Request<{ id: string }>,
+  res: Response
+) => {
+  try {
+    const { name, email, password, type } = req.body;
+    if (!name || name === "") {
+      return res.status(400).json({ error: "Name is required" });
+    }
+
+    const user = await UserModel.findByPk(req.params.id);
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    user.name = name;
+
+    await user.save();
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json("Erro interno no servidor " + error);
+  }
 };
--- Arte_Da_Guerra/src/routes/UserRouter.ts
+++ Arte_Da_Guerra/src/routes/UserRouter.ts
@@ -1,10 +1,16 @@
 import express from "express";
-import { createUser, getAll, getUserById } from "../controllers/UserController";
+import {
+  createUser,
+  getAll,
+  getUserById,
+  updateUser,
+} from "../controllers/UserController";
 
 const router = express.Router();
 
 router.get("/users", getAll);
 router.get("/users/:id", getUserById);
 router.post("/users", createUser);
+router.put("/users/:id", updateUser);
 
 export default router;
