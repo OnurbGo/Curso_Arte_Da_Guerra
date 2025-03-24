8706e9601eed66ac7e5075269d893b8ecddb54a9
OnurbGo <bruno.baltieri@grupointegrado.br>
2025-03-24 10:31:34 -0300

    Teste De Imagens

--- Front_End/FrontEnd/my-app/src/pages/Login.tsx
+++ Front_End/FrontEnd/my-app/src/pages/Login.tsx
@@ -119,11 +119,7 @@ export default function Login() {
           <div className="flex lg:flex-1">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
-              <img
-                alt=""
-                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-                className="h-8 w-auto"
-              />
+              <img alt="Yin and Yang" src="" className="h-8 w-auto" />
             </a>
           </div>
           <div className="flex lg:hidden">
